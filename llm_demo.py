795a09f556189a71e190997b40db61b235408c27
Paul Lerner <lerner@tuta.io>
2024-09-07 14:13:10 +0200

    refactor: LLM demo: remove commented debugging code

--- llm_demo.py
+++ llm_demo.py
@@ -8,31 +8,27 @@ def pipeline(text, model, tokenizer):
     for k, v in inputs.items():
         inputs[k] = v.cuda()
     input_length = inputs["input_ids"].shape[1]
-    output = model.generate(**inputs, max_new_tokens=128)#, pad_token_id=tokenizer.eos_token_id)#, max_new_tokens=32
+    output = model.generate(**inputs, max_new_tokens=128)
     output = tokenizer.batch_decode(output[:, input_length:], skip_special_tokens=True, clean_up_tokenization_spaces=True)
     return output
 
 
 def user_loop(model, tokenizer):
     while True:
-        answer = input(f">>> ").strip()#"what is aivancity?" #
+        answer = input(f">>> ").strip()
         output = pipeline(answer,model, tokenizer)[0]
         print(f"{output}\n")
 
 
 def main():
-    model_name = "google/gemma-2-2b-it"#/home/paul/code/neot/data/models/Meta-Llama-3.1-8B-Instruct"
+    model_name = "google/gemma-2-2b-it"
     model  = AutoModelForCausalLM.from_pretrained(
         model_name,
         device_map="auto",
         token=True,
         quantization_config=BitsAndBytesConfig(load_in_4bit=True)
-            #load_in_8bit=True, llm_int8_enable_fp32_cpu_offload=True)
-    )#low_cpu_mem_usage=True
-    #print(f"{model.config.max_length=}")
+    )
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    #tokenizer.pad_token = tokenizer.eos_token
-    #tokenizer.pad_token_id = tokenizer.eos_token_id
     user_loop(model, tokenizer)
 
 
